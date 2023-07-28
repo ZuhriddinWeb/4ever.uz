e9b66151303e23dc37c55ec830bb9bf0a843534f
Zuhriddin <zuhriddin_mustafoyev@mail.ru>
2023-07-28 17:40:42 +0500

    payment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Orders;
+use Http;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +24,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
-    }
+    //    $query =  Http::withHeaders([
+    //         'Content-Language' => 'uz-UZ',
+    //         'X-Fingerprint' => '355ecee8b55693deccf2c9461415228ba9c80d38',
+    //         'X-Signature'=>'MEUCIQC8loCVlc/akDRdaVujJPQg8gMcVebSg5nerEET0X/mVQIgQrRbMQcr8mWeIxwtNt3eG44jA5sHOcFS2jHyxq6Flww=',
+    //         'X-Terminal-Id' => '52ace782-f92f-47d6-8789-39ebd8066e59'
+    //     ])->post('https://test-chk-api.ipt-merch.com/api/v1/payment/register', [
+    //         'amount' => 100,
+    //         'clientId' => 2,
+    //         'currency' => 643,
+    //         'orderNumber' => 1,
+    //         'viewType' => 'WEB_VIEW',
+    //         'paymentParams' => [
+    //             'payType' => 'TWO_STEP'
+    //         ],
+    //         'sessionTimeoutSecs' => 1000,
+    //     ]);
+    //     $json = json_decode($query, true);
+
+    //     dd($json);
+     }
 }
--- public/js/resources_js_pages_Register_vue.js
+++ public/js/resources_js_pages_Register_vue.js
@@ -298,13 +298,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _Register_vue_vue_type_template_id_364a2fac__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Register.vue?vue&type=template&id=364a2fac */ "./resources/js/pages/Register.vue?vue&type=template&id=364a2fac");
 /* harmony import */ var _Register_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Register.vue?vue&type=script&setup=true&lang=js */ "./resources/js/pages/Register.vue?vue&type=script&setup=true&lang=js");
-/* harmony import */ var D_media_ospanel_domains_4ever_uz_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var E_xampp_htdocs_online_shop_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,D_media_ospanel_domains_4ever_uz_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_Register_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_Register_vue_vue_type_template_id_364a2fac__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/pages/Register.vue"]])
+const __exports__ = /*#__PURE__*/(0,E_xampp_htdocs_online_shop_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_Register_vue_vue_type_script_setup_true_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_Register_vue_vue_type_template_id_364a2fac__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/pages/Register.vue"]])
 /* hot reload */
 if (false) {}
 
