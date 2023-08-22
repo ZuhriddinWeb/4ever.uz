d2dee732cde31090ddc19137f2f6942acf90895d
Zuhriddin <zuhriddin_mustafoyev@mail.ru>
2023-08-22 15:11:43 +0500

    changed slider

--- resources/js/components/Slider.vue
+++ resources/js/components/Slider.vue
@@ -3,7 +3,7 @@
         <swiper class="rounded-md border" :slides-per-view="1" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
             <swiper-slide>
                 <main class="h-[50vh]">
-                    <div class="h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('https://i.ibb.co/59bQX9m/slider.jpg');">
+                    <div class="h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style="background-image: url('https://i.ibb.co/0jw0BqB/df18a08e-32d2-470a-909e-6a4e0b401085.jpg');">
                         <div
                             class="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
                             <!-- <p class="text-black text-2xl my-4">ForeverGAB</p> -->
