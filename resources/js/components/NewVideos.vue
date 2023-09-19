a3b03c65dc030b15eec3ddfdad27bcc41cd5a729
Zuhriddin <zuhriddin_mustafoyev@mail.ru>
2023-09-19 16:24:05 +0500

    videos deleted

--- resources/js/components/NewVideos.vue
+++ resources/js/components/NewVideos.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="py-8">
+    <!-- <section class="py-8">
         <div class="flex items-center flex-wrap">
             <nav id="store" class="w-full z-30 top-0">
                 <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-1.5">
@@ -10,15 +10,15 @@
             </nav>
 
             <main class="flex flex-wrap w-full -mx-2">
-                <!-- <video class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-48 flex flex-col px-2" controls>
+                <video class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-48 flex flex-col px-2" controls>
                     <source src="/images/IMG_5588.mov" type="video/mp4">
-                </video> -->
+                </video>
                 <video class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-48 flex flex-col px-2 my-2" controls>
                     <source src="/images/tour.mp4" type="video/mp4">
                 </video>
             </main>
         </div>
-    </section>
+    </section> -->
 </template>
   
 <script setup>
